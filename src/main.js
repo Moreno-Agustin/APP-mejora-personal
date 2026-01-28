afa6597483685701b2a26f284de39c79571f35d4
Agustin <agustinmooreno07@gmail.com>
2026-01-28 15:34:29 -0300

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// ==========================
+// 1. DASHBOARD LOGIC
+// ==========================
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.presentation-card');
+    const nextButtons = document.querySelectorAll('.btn-next');
+    const presentation = document.querySelector('.presentation');
+    let currentCard = 0;
+    nextButtons.forEach((btn) => {
+        btn.addEventListener('click', () => {
+            // Hide current card
+            cards[currentCard].classList.remove('active');
+            currentCard++;
+            if (currentCard < cards.length) {
+                // Show next card
+                cards[currentCard].classList.add('active');
+            }
+            else {
+                // End presentation
+                if (presentation) {
+                    presentation.style.display = 'none';
+                }
+            }
+        });
+    });
+});
+//# sourceMappingURL=main.js.map
