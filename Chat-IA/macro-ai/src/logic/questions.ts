74718ef28e893d87f9b46164396ac39d726b11e0
Agustin <agustinmooreno07@gmail.com>
2026-01-30 15:54:26 -0300

    Delete questions.ts

--- Chat-IA/macro-ai/src/logic/questions.ts
+++
@@ -1,8 +0,0 @@
-import type { UserProfile } from "../types/types";
-
-export function getNextQuestion(user: UserProfile): string | null {
-    if (user.weight == null) {
-        return "¿Cuánto pesás actualmente? (en kg)";
-    }
-    return null;
-}
