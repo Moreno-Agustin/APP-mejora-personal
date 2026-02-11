358d3f625ecddabc58f62148ec454d4a7802002e
Agustin <agustinmooreno07@gmail.com>
2026-02-11 17:51:47 -0300

    Delete memory.js

--- Chat-IA/macro-ai/src/Js/data/memory.js
+++
@@ -1,8 +0,0 @@
-const KEY = "sports_ai_user";
-export function loadUser() {
-    const data = localStorage.getItem(KEY);
-    return data ? JSON.parse(data) : null;
-}
-export function saveUser(user) {
-    localStorage.setItem(KEY, JSON.stringify(user));
-}
