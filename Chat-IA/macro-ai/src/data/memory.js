6a23129dda5d1244e5e2eb15ffa737f7529659db
Agustin <agustinmooreno07@gmail.com>
2026-02-11 17:46:03 -0300

    Create memory.js

---
+++ Chat-IA/macro-ai/src/data/memory.js
@@ -0,0 +1,9 @@
+const KEY = "sports_ai_user";
+export function loadUser() {
+    const data = localStorage.getItem(KEY);
+    return data ? JSON.parse(data) : null;
+}
+export function saveUser(user) {
+    localStorage.setItem(KEY, JSON.stringify(user));
+}
+//# sourceMappingURL=memory.js.map
